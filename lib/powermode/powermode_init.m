0f1f2a57bcf5b7da5c30ab5c3b8f8994e5d7d980
Luis Ernesto Suarez Velazquez <luis-ernesto.suarez-velazquez@s2015.tu-chemnitz.de>
2017-04-11 17:55:49 +0200

    Sensors rearranged.

--- lib/powermode/powermode_init.m
+++ lib/powermode/powermode_init.m
@@ -4,9 +4,11 @@
 %
 
 %% Power Mode variables
-SteeringWheelAngle = 0;
-BrakeTravel = 0;
-IgnitionStatus = 0;
-aiFr_PDO2_Timeout_Status = 0;
-aiRe_PDO2_Timeout_Status = 0;
-Battery_Temperature_Status = 0;
+% SteeringWheelAngle = 0;
+% BrakeTravel = 0;
+% IgnitionStatus = 0;
+% aiFr_PDO2_Timeout_Status = 0;
+% aiRe_PDO2_Timeout_Status = 0;
+% Battery_Temperature_Status = 0;
+
+Ignition_Status = IgnitionStatus.Undefined;
