8a24449f8f2acd7d63bafb9758ac95236e7de23b
Luis <luersuve@gmail.com>
2017-11-09 18:37:30 +0100

    Diagnostics updated.

---
+++ emulation_settings.m
@@ -0,0 +1,25 @@
+% Emulation Configuration Settings
+TP_Enable_Emulation = false;
+TP1_Enable_Emulation = false;
+TP2_Enable_Emulation = false;
+BP1_Enable_Emulation = false;
+BP2_Enable_Emulation = false;
+SWS_Enable_Emulation = false;
+WSFL_Enable_Emulation = false;
+WSFR_Enable_Emulation = false;
+WSRL_Enable_Emulation = false;
+WSRR_Enable_Emulation = false;
+
+
+% Emulation Values
+Emu_Steering_Angle = 0.0;
+Emu_Throttle_Pedal = 0.0;
+Emu_Throttle_Pedal_1 = 0.0;
+Emu_Throttle_Pedal_2 = 0.0;
+Emu_Brake_Pedal_1 = 0.0;
+Emu_Brake_Pedal_2 = 0.0;
+
+Emu_Wheel_Speed_FL = 0.0;
+Emu_Wheel_Speed_FR = 0.0;
+Emu_Wheel_Speed_RL = 0.0;
+Emu_Wheel_Speed_RR = 0.0;
